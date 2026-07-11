53e3a61d4c08a6f07e8db4ca46da5c140ed4e7d0
d44niilsolyanoy <d4niilsolyaoi@gmail.com>
2026-07-11 12:12:55 +0300

    Исправлена обработка ctrl+c

--- lib/menu.sh
+++ lib/menu.sh
@@ -3,7 +3,6 @@
 confirm_action() {
     local title="$1"
     local description="$2"
-    local interrupted=0
     local read_status
 
     clear
@@ -11,12 +10,12 @@ confirm_action() {
     printf "%s\n" "$description"
     printf "\n${CYAN}Нажимте Enter чтобы продолжить${NC}"
 
-    trap 'interrupted=1' INT
+    trap 'trap - INT; printf "\n"; info "Действие отменено. Возврат в предыдущее меню."; return 130' INT
     read -r
     read_status=$?
     trap - INT
 
-    if [ "$interrupted" -eq 1 ] || [ "$read_status" -ne 0 ]; then
+    if [ "$read_status" -ne 0 ]; then
         printf "\n"
         info "Действие отменено. Возврат в предыдущее меню."
         return 1
