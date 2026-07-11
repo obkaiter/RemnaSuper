78443879d2d4fd02d9d1571a95a6c8d756e869da
d44niilsolyanoy <d4niilsolyaoi@gmail.com>
2026-07-11 12:02:38 +0300

    Вернул установку geofiles

--- lib/menu.sh
+++ lib/menu.sh
@@ -20,6 +20,66 @@ run_action() {
     "$action"
 }
 
+run_geofiles_action() {
+    local title="$1"
+    local description="$2"
+    local action="$3"
+
+    confirm_action "$title" "$description"
+    "$action"
+    pause
+}
+
+show_geofiles_menu() {
+    clear
+    show_brand "Geofiles"
+
+    section "Установка"
+    menu_item 1 "Установить Loyalsoldier-geofiles"
+    menu_item 2 "Установить xray-routing geofiles"
+    menu_item 3 "Установить все geofiles"
+
+    section "Удаление"
+    menu_danger_item 4 "Удалить Loyalsoldier-geofiles"
+    menu_danger_item 5 "Удалить xray-routing geofiles"
+    menu_danger_item 6 "Удалить все geofiles"
+
+    section "Навигация"
+    menu_back_item
+    prompt_choice "0-6"
+}
+
+geofiles_menu() {
+    local choice
+
+    while true; do
+        show_geofiles_menu
+        read -r choice
+        case $choice in
+            1) run_geofiles_action "Установка Loyalsoldier-geofiles" \
+                "Будут скачаны geosite.dat и geoip.dat проекта Loyalsoldier, подключены к RemnaNode и добавлены в автоматическое еженедельное обновление." \
+                install_loyalsoldier_geofiles ;;
+            2) run_geofiles_action "Установка xray-routing geofiles" \
+                "Будут скачаны geosite.dat и geoip.dat проекта xray-routing, подключены к RemnaNode и добавлены в автоматическое еженедельное обновление." \
+                install_xray_routing_geofiles ;;
+            3) run_geofiles_action "Установка всех geofiles" \
+                "Будут установлены наборы Loyalsoldier и xray-routing, подключены к RemnaNode и добавлены в автоматическое еженедельное обновление." \
+                install_all_geofiles ;;
+            4) run_geofiles_action "Удаление Loyalsoldier-geofiles" \
+                "Файлы Loyalsoldier будут удалены, их volumes исключены из docker-compose.yml, а задание автоматического обновления будет удалено." \
+                uninstall_loyalsoldier_geofiles ;;
+            5) run_geofiles_action "Удаление xray-routing geofiles" \
+                "Файлы xray-routing будут удалены, их volumes исключены из docker-compose.yml, а задание автоматического обновления будет удалено." \
+                uninstall_xray_routing_geofiles ;;
+            6) run_geofiles_action "Удаление всех geofiles" \
+                "Все установленные наборы geofiles, их volumes и задания автоматического обновления будут удалены." \
+                uninstall_all_geofiles ;;
+            0) return ;;
+            *) warn "Неверный выбор."; sleep 1 ;;
+        esac
+    done
+}
+
 show_menu() {
     clear
     show_brand "Главное меню"
@@ -40,9 +100,12 @@ show_menu() {
     menu_item 12 "Просмотр ошибок Xray"
     menu_item 13 "Просмотр логов Xray"
 
+    section "Прочее"
+    menu_item 14 "Установка/удаление geofiles"
+
     section "Навигация"
     menu_exit_item
-    prompt_choice "0-2, 4-13"
+    prompt_choice "0-2, 4-14"
 }
 
 main_menu() {
@@ -88,6 +151,7 @@ main_menu() {
             13) run_action "Просмотр логов Xray" \
                 "Будет выполнена команда tail -f /var/log/remnanode/access.log. Для выхода нажмите Ctrl+C." \
                 view_xray_logs ;;
+            14) geofiles_menu ;;
             0) run_action "Выход" \
                 "Работа RemnaSuper будет завершена." \
                 exit_menu ;;
